f5502b5a67cf85327d93d7ea671efaa8ecfbcc02
MyABKit <bulla.anandkumar@gmail.com>
2023-09-03 06:27:55 +0530

    updated the file

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,14 @@ public class HelloWorld {
 	System.out.print(" I am default constructor");
 	}
 
+  public void M1(){
+  	System.out.print("In this method you can process the items");
+  }
   public static void main(String[] args) {
 	System.out.println("Hello, World");
         System.out.println("Hello, I am got appende World");
+	Hellowworld hw= new Hellowworld();
+		hw.M1();
 
   }
 }
